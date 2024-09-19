17260593016701b4b6412521dcc88ba661b95c20
Cristtsirc <cristian.sangucho@epn.edu.ec>
2024-09-18 21:41:01 -0500

    initial commit

---
+++ intro/index.js
@@ -0,0 +1,19 @@
+// * conceptos de javascript
+
+// Variables
+var example = "a"            //Legacy
+let aux = "b"            
+
+// Constants
+const PI = 3.1416
+const exampl = 3454
+PI = 3
+console.log(example, aux, PI, number)
+
+//Data types
+let string = "string"
+let number = 1
+let boolean = true
+let nullvalue = null
+//let undefinedvalue = undefined
+
